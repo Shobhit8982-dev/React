bea3cefaa42fce0abb72746c68ab97491bf15337
Shobhit8982-dev <sahu.shobhit2801@gmail.com>
2025-07-21 19:38:49 +0530

    session 1

---
+++ app.js
@@ -0,0 +1,33 @@
+
+    // const heading = React.createElement('h1', {id:'heading'}, 'hello from react'); 
+    // const root = ReactDOM.createRoot(document.getElementById('root'));
+    // root.render(heading);
+
+    // console.log(heading) //gives an object
+
+// <div id='parent'>
+//     <div id='child'>
+//         <h1>i am h1 tag</h1>
+//         <h1>I am h1 tag</h1>
+//     </div>
+//     <div id='child2'>
+//         <h1>i am h1 tag</h1>
+//         <h1>I am h1 tag</h1>
+//     </div>
+// </div>
+
+// React element(object) => HTML (Browser understands)
+
+const header = React.createElement('div', {id:'parent'}, [
+    React.createElement('div',{id:'child'},[
+        React.createElement('h1',{},'I am a H1 Tag'),
+        React.createElement('h1',{},'I am a H1 Tag')
+    ]),
+    React.createElement('div',{id:'child2'},[
+        React.createElement('h1',{},'I am a H1 Tag'),
+        React.createElement('h1',{},'I am a H1 Tag')
+    ])
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('header'));
+root.render(header);
