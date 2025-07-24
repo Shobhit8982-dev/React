18a42ec9c742d20551d6766aa93c6b516a65b508
Administrator <administrator@Administrators-MacBook-Pro.local>
2025-07-24 23:10:54 +0530

    session 2

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
+import React from "react";
+import ReactDOM from 'react-dom/client'
     // const heading = React.createElement('h1', {id:'heading'}, 'hello from react'); 
     // const root = ReactDOM.createRoot(document.getElementById('root'));
     // root.render(heading);
@@ -18,16 +19,30 @@
 
 // React element(object) => HTML (Browser understands)
 
-const header = React.createElement('div', {id:'parent'}, [
-    React.createElement('div',{id:'child'},[
-        React.createElement('h1',{},'I am a H1 Tag'),
-        React.createElement('h1',{},'I am a H1 Tag')
-    ]),
-    React.createElement('div',{id:'child2'},[
-        React.createElement('h1',{},'I am a H1 Tag'),
-        React.createElement('h1',{},'I am a H1 Tag')
-    ])
-]);
+// const header = React.createElement('div', {id:'parent'}, [
+//     React.createElement('div',{key:'c1',id:'child'},[
+//         React.createElement('h1',{key:'1'},'I am a H1 Tag'),
+//         React.createElement('h1',{key:'2'},'I am a H1 Tag')
+//     ]),
+//     React.createElement('div',{key:'c2',id:'child2'},[
+//         React.createElement('h1',{key:'3'},'I am a H1 Tag'),
+//         React.createElement('h1',{key:'4'},'I am a H1 Tag')
+//     ])
+// ]);
 
-const root = ReactDOM.createRoot(document.getElementById('header'));
-root.render(header);
+const Header = () => (
+  <div id="parent">
+    <div id="child">
+      <h1 key="h1-1">I am a H1 Tag</h1>
+      <h1 key="h1-2">I am a H1 Tag</h1>
+    </div>
+    <div id="child2">
+      <h1 key="h1-3">I am a H1 Tag</h1>
+      <h1 key="h1-4">I am a H1 Tag</h1>
+    </div>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('Header'));
+// root.render(Header);
+root.render(<Header />)
