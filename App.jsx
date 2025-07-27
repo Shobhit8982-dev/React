6ca838e8d1ffd03bb5f5da1e724986ad8f310788
Shobhit8982-dev <sahu.shobhit2801@gmail.com>
2025-07-27 14:09:37 +0530

    intro

--- App.jsx
+++ App.jsx
@@ -1,4 +1,6 @@
-
+ import React from 'react';
+ import ReactDOM from 'react-dom/client';
+ import App from './App.jsx';
     // const heading = React.createElement('h1', {id:'heading'}, 'hello from react'); 
     // const root = ReactDOM.createRoot(document.getElementById('root'));
     // root.render(heading);
